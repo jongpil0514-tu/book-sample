a5ff8729091c28954be6d0bd246733f090cd2317
hiphop9n <hiphop9n@gmail.com>
2025-04-08 15:32:24 +0900

    first commit

---
+++ src/main/java/com/framework/BookSampleApplication.java
@@ -0,0 +1,13 @@
+package com.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSampleApplication.class, args);
+		System.out.println("Hello World");
+	}
+}
